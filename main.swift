32774624676b46118b3331eabce8ed6b442cd5a2
Biddybuggy <100064835+Biddybuggy@users.noreply.github.com>
2022-07-09 21:29:11 +0700

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,1 @@
+print("\nNever gonna give you up \nNever gonna let you down \nNever gonna run around and desert you \nNever gonna make you cry \nNever gonna say goodbye \nNever gonna tell a lie and hurt you")
